c2ed738736a03bcd3ab75e848c1fe32cc5f308be
Clement Calenge <clement.calenge@gmail.com>
2015-03-26 21:24:00 +0100

    Commit Final Version 0.3.9

--- R/explore.r
+++ R/explore.r
@@ -72,9 +72,6 @@ explore <- function (ka, coltxt="black",
     opt <- options(warn = -1)
     on.exit(options(opt))
 
-    if (!require(tkrplot))
-        stop("'tkrplot' package needed\n")
-
     help.txt <- paste("\n-------- to obtain this help, type 'h' ------------------",
                       "z/o            -- Zoom in/Out",
                       "Right-Click    -- Identify value",
@@ -84,23 +81,23 @@ explore <- function (ka, coltxt="black",
                       "---------------------------------------------------------",
                       "\n", sep = "\n")
 
-    tt <- tktoplevel()
-    tkwm.title(tt, "Exploration of SpatialPixelsDataFrame")
-    img <- tkrplot(tt, replot, hscale = hscale, vscale = vscale)
-    txt <- tktext(tt, bg = "white", font = "courier 10")
-    scr <- tkscrollbar(tt, repeatinterval = 5, command = function(...) tkyview(txt, ...))
-    tkconfigure(txt, yscrollcommand = function(...) tkset(scr,
-                     ...))
-    tkpack(img, side = "top")
-    tkpack(txt, side = "left", fill = "both", expand = TRUE)
-    tkpack(scr, side = "right", fill = "y")
-    iw <- as.numeric(tcl("image", "width", tkcget(img, "-image")))
-    ih <- as.numeric(tcl("image", "height", tkcget(img, "-image")))
+    tt <- tcltk::tktoplevel()
+    tcltk::tkwm.title(tt, "Exploration of SpatialPixelsDataFrame")
+    img <- tkrplot::tkrplot(tt, replot, hscale = hscale, vscale = vscale)
+    txt <- tcltk::tktext(tt, bg = "white", font = "courier 10")
+    scr <- tcltk::tkscrollbar(tt, repeatinterval = 5, command = function(...) tcltk::tkyview(txt, ...))
+    tcltk::tkconfigure(txt, yscrollcommand = function(...) tcltk::tkset(scr,
+                            ...))
+    tcltk::tkpack(img, side = "top")
+    tcltk::tkpack(txt, side = "left", fill = "both", expand = TRUE)
+    tcltk::tkpack(scr, side = "right", fill = "y")
+    iw <- as.numeric(tcltk::tcl("image", "width", tcltk::tkcget(img, "-image")))
+    ih <- as.numeric(tcltk::tcl("image", "height", tcltk::tkcget(img, "-image")))
 
-    showz <- function() tkrreplot(img)
+    showz <- function() tkrplot::tkrreplot(img)
     type <- function(s) {
-        tkinsert(txt, "end", s)
-        tksee(txt, "end")
+        tcltk::tkinsert(txt, "end", s)
+        tcltk::tksee(txt, "end")
     }
     type(help.txt)
     cc <- function(x, y) {
@@ -186,7 +183,7 @@ explore <- function (ka, coltxt="black",
     kb <- function(A) {
         key <- tolower(A)
         if (key == "q") {
-            tkdestroy(tt)
+            tcltk::tkdestroy(tt)
             return("OK - Finished")
         }
         if (key == "z") {
@@ -203,10 +200,10 @@ explore <- function (ka, coltxt="black",
         }
     }
     showz()
-    tkbind(tt, "<Key>", kb)
-    tkbind(img, "<Button-1>", mm.t)
-    tkbind(img, "<Motion>", mm.mouset)
-    tkbind(img, "<Button-3>", mm.t2)
-    tkwait.window(tt)
+    tcltk::tkbind(tt, "<Key>", kb)
+    tcltk::tkbind(img, "<Button-1>", mm.t)
+    tcltk::tkbind(img, "<Motion>", mm.mouset)
+    tcltk::tkbind(img, "<Button-3>", mm.t2)
+    tcltk::tkwait.window(tt)
 
 }
